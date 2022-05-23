afff1e7a360513c0e83da17d186bdbc839f66e7f
MontanhaCs <iago18052006@gmail.com>
2022-05-22 21:18:38 -0300

    Game 2.3
    Mudança do design do personagem (Não finalizado)

--- Assets/Scripts/movimento.cs
+++ Assets/Scripts/movimento.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 public class movimento : MonoBehaviour
 {
 
-  public Animator animator;
+  
   public float movementSpeed =1;
     public float jumpForce =1;
     private Rigidbody2D _rigidbody;
+    public Animator animator;
 
     private float horizontal;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
    private void Update()
     {
-
+        
       var movement = Input.GetAxis("Horizontal");
 
       transform.position += new Vector3(movement,0,0) * Time.deltaTime * movementSpeed;
-      
+      if(Input.GetAxis("Horizontal")!=0){
+            //movendo
+            animator.SetBool("taMovendo", true);
+        }else{
+          //parado
+          animator.SetBool("taMovendo", false);
+        }
     
      if(!Mathf.Approximately(0,movement))
      transform.rotation= movement < 0 ? Quaternion.Euler(0,180,0) : Quaternion.identity;
