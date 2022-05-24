666fbf67ae7cb7fa45177c31bb2e85d33ffbf84e
MontanhaCs <iago18052006@gmail.com>
2022-05-23 23:06:52 -0300

    Game 2.5
    Um chão para o personagem ao cair, ele irá respawnar

--- Assets/Scripts/morte.cs
+++ Assets/Scripts/morte.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class morte : MonoBehaviour
 {
+    
+void OnTriggerEnter2D(Collider2D other)
+    {
+        
+                SceneManager.LoadScene("SampleScene");
+                Debug.Log("batendo");
+    }
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,5 @@ public class morte : MonoBehaviour
     {
         
     }
+    
 }
